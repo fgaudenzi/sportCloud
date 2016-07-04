9e998c3f858a4fb4b87bd4f86b0d421fc270fbee
Filippo Gaudenzi <gaudenzi.filippo@gmail.com>
2016-07-04 16:51:10 +0200

    cors

--- flaskapp.py
+++ flaskapp.py
@@ -7,7 +7,6 @@ from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
-CORS(app)
 app.config.from_pyfile('flaskapp.cfg')
 
 
--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='FlaskApp',
       author='Ryan Jarvinen',
       author_email='ryanj@redhat.com',
       url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
+     install_requires=['Flask>=0.10.1','Flask-Cors>=2.1.2'],
      )
